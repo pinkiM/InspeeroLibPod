ffd7ce814cba09f93c9c381b944aabb6e65532bb
Pinki Maurya <pmaurya@inspeero.com>
2015-10-12 18:47:28 +0530

    First Commit

---
+++ InspeeroLibPod/InspeeroLibPod.h
@@ -0,0 +1,13 @@
+//
+//  InspeeroLibPod.h
+//  InspeeroLibPod
+//
+//  Created by MacBook Pro on 12/10/15.
+//  Copyright © 2015 inspeero technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InspeeroLibPod : NSObject
+
+@end
---
+++ InspeeroLibPod/InspeeroLibPod.m
@@ -0,0 +1,13 @@
+//
+//  InspeeroLibPod.m
+//  InspeeroLibPod
+//
+//  Created by MacBook Pro on 12/10/15.
+//  Copyright © 2015 inspeero technologies. All rights reserved.
+//
+
+#import "InspeeroLibPod.h"
+
+@implementation InspeeroLibPod
+
+@end
